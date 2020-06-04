b1b178e287897560f9769a2dcb6243c18993e016
Manu <sahumanu021@gmail.com>
2020-06-04 10:08:59 +0530

    Demo Algorithms App

---
+++ app/src/main/java/com/stark98/kaal/MainActivity.java
@@ -0,0 +1,51 @@
+package com.stark98.kaal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void prm_number(View view) {
+    }
+
+    public void rabin_sort(View view) {
+    }
+
+    public void cuntng_srt(View view) {
+        Toast.makeText(this, "App is in under Development", Toast.LENGTH_SHORT).show();
+    }
+
+    public void kmp_algo(View view) {
+    }
+
+    public void bble_srt(View view) {
+    }
+
+    public void bnry_srt(View view) {
+    }
+
+    public void fnbci_sres(View view) {
+    }
+
+    public void linr_srt(View view) {
+    }
+
+    public void mrg_srt(View view) {
+    }
+
+    public void cmpst_sers(View view) {
+    }
+
+    public void quk_srt(View view) {
+
+    }
+}
