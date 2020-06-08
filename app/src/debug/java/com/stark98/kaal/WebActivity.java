ab448628a24c87a78fb10af4eac92fe1f95c70fb
Manu <sahumanu021@gmail.com>
2020-06-08 20:58:11 +0530

    Demo Algorithms App

--- app/src/debug/java/com/stark98/kaal/WebActivity.java
+++ app/src/debug/java/com/stark98/kaal/WebActivity.java
@@ -1,14 +1,69 @@
 package com.stark98.kaal;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class WebActivity extends AppCompatActivity {
+
+    WebView webView;
+    private String webUrl = "https://www.geeksforgeeks.org";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(FourActivity.Extra_Message1);
+        switch (message) {
+            case "link2": {
+                webUrl = "";
+            }
+            case "link3": {
+                webUrl = "";
+            }
+            case "link4": {
+                webUrl = "";
+            }
+            case "link5": {
+                webUrl = "";
+            }
+            case "link6": {
+                webUrl = "";
+            }
+            case "link7": {
+                webUrl = "";
+            }
+            case "link8": {
+                webUrl = "";
+            }
+            case "link9": {
+                webUrl = "";
+            }
+            case "link10": {
+                webUrl = "";
+            }
+            case "link11": {
+                webUrl = "";
+            }
+            case "link12": {
+                webUrl = "";
+            }
+        }
+        webView = (WebView) findViewById(R.id.mywebView);
+        webView.loadUrl(webUrl);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                url = webUrl;
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+
     }
 }
